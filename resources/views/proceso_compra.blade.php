7dc0bc687cc78bed3fe2ad9f10fa8ef706a06421
brenik <breniksantiago@gmail.com>
2025-10-07 00:49:13 -0500

    correccion de fontend FINAL FINAL

--- resources/views/proceso_compra.blade.php
+++ resources/views/proceso_compra.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GogoShop - Checkout</title>
-    <link rel="icon" type="image/x-icon" href="../images/iconGOGO.png">
+    <title>CarniClick - Checkout</title>
+    <link rel="icon" type="image/x-icon" href="../images/logo.png">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/payment.css')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
